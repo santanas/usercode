521052583d51ded548f0c90cb25ea150f172c42b
santanas <>
2010-10-27 08:29:18 +0000

    added MC file relative to Spring10 production

--- Various/MyEcalChannelStatus/myecalchannelstatus_cfg.py
+++ Various/MyEcalChannelStatus/myecalchannelstatus_cfg.py
@@ -16,13 +16,15 @@ fileName = cms.string("EcalDeadChannels.root")
 )
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'GR10_P_V10::All'
+#process.GlobalTag.globaltag = 'GR10_P_V10::All'
+process.GlobalTag.globaltag = 'START3X_V26::All'
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        '/store/data/Run2010B/Electron/RECO/PromptReco-v2/000/147/450/A2DED983-7BD3-DF11-9C4B-001617C3B6FE.root'
-#        'file:myfile.root'
+    #        '/store/data/Run2010B/Electron/RECO/PromptReco-v2/000/147/450/A2DED983-7BD3-DF11-9C4B-001617C3B6FE.root'
+    '/store/relval/CMSSW_3_5_7/RelValTTbar/GEN-SIM-RECO/START3X_V26-v1/0012/F8624D39-5349-DF11-A757-001A92971B36.root'
+    #        'file:myfile.root'
     )
 )
 
