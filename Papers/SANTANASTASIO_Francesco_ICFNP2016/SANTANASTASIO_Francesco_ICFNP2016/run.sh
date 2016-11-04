b9237595dde043aeacfc31cd3d4b4ae927c531ad
Francesco Santanastasio <santanas@dhwired13131.roma1.infn.it>
2016-11-04 17:29:44 +0100

    added ICFNP2016 proceedings

---
+++ Papers/SANTANASTASIO_Francesco_ICFNP2016/SANTANASTASIO_Francesco_ICFNP2016/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pdflatex SANTANASTASIO_Francesco_ICFNP2016.tex
+pdflatex SANTANASTASIO_Francesco_ICFNP2016.tex
+#pdflatex SANTANASTASIO_Francesco_ICFNP2016.tex
+open SANTANASTASIO_Francesco_ICFNP2016.pdf
+
+
