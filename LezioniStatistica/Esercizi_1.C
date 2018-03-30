e1d2f311db232a0c2ac7cf6d4a679cdec8b6a9c3
Francesco Santanastasio <santanas@FrancescosMBP2.lan>
2018-03-30 17:48:34 +0200

    esercizio temperatura giorno-notte

---
+++ LezioniStatistica/Esercizi_1.C
@@ -0,0 +1,38 @@
+{
+
+  //  gStyle->SetOptStat(0);
+
+  const int n = 20;
+  double T1[n]={11.3 , 11.6 , 11.4 , 12.1 , 11.1 , 12.2 , 11.5 , 11.8 , 11.7 , 12.0 , 11.1 , 12.1 , 11.9 , 11.4 , 11.7 , 11.6 , 12.0 , 11.4 , 11.7 , 11.3 };
+  double T2[n]={11.1 , 10.9 , 11.4 , 11.3 , 10.8 , 11.1 , 11.3 , 11.4 , 11.0 , 11.3 , 11.0 , 11.0 , 11.4 , 10.9 , 11.2 , 11.3 , 11.3 , 11.1 , 11.0 , 11.3 };
+  float Tmin = 10.45;
+  float Tmax = 12.55;
+  float binSize = 0.1; 
+  int Nbins = int((Tmax - Tmin) / binSize); 
+
+  TCanvas c1;
+  TH1F *h1_T1 = new TH1F("h1_T1","h1_T1",Nbins,Tmin,Tmax);  
+  h1_T1->SetTitle("");
+  h1_T1->GetXaxis()->SetTitle("Temperatura [C]");
+  h1_T1->GetYaxis()->SetTitle("numero di misure = entries");
+  TH1F *h1_T2 = new TH1F("h1_T2","h1_T2",Nbins,Tmin,Tmax);  
+  h1_T2->SetTitle("");
+  h1_T2->SetLineColor(2);
+  h1_T2->GetXaxis()->SetTitle("Temperatura [C]");
+  h1_T2->GetYaxis()->SetTitle("numero di misure = entries");
+  for(int k=0; k<n;k++)
+    {
+      h1_T1->Fill(T1[k]);
+      h1_T2->Fill(T2[k]);
+    }
+  h1_T1->Draw();
+  h1_T1->GetYaxis()->SetRangeUser(0,8);
+  h1_T2->Draw("sames");
+  binSize = (Tmax - Tmin) / Nbins; 
+  TPaveText *pt1=new TPaveText(0.72,0.72,0.95,0.87,"brNDC");
+  pt1->AddText(Form("N bins = %d",Nbins));
+  pt1->AddText(Form("Bin size = %3.4f [C]",binSize));
+  pt1->Draw();
+  c1.SaveAs("h_Temperatures.pdf");
+
+}
